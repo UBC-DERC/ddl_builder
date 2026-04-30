ec81a011c352141ead11f0a25312d8ed8efb10a3
Simon <simon.j.goring@gmail.com>
2026-04-29 17:31:43 -0700

    Initial push for the DDL builder.
    The DDL builder is intended to connect to a postgres database to begin building the database from the YAML.

---
+++ build_model.py
@@ -0,0 +1,14 @@
+import ddl_builder as dlb
+
+# First we want to point to the appropriate directory
+yaml_model = dlb.read_files('../data_model')
+print(yaml_model)
+
+
+# Then we build the full yaml model
+
+# Ideally we'd then output as mermaid or some sort of flow chart
+
+# Build some osrt of documentation
+
+# And create the database model
---
+++ src/ddl_builder/read_files.py
@@ -0,0 +1,9 @@
+import yaml
+from pathlib import Path
+
+def read_files(filepath:str)->object:
+    path = Path(filepath)
+    assert path.exists(), f"The path provided ({filepath}) is not a valid path/file/directory."
+    assert path.is_dir(), "The path provided to read_files must be a directory."
+    file_list = [child for child in path.iterdir()]
+    return file_list
